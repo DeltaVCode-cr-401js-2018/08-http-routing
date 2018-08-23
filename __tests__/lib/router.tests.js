b05cfd56fc297e92e8595c08d7304a0ede1a0641
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-22 22:11:03 -0500

    adding routes to tester

--- __tests__/lib/router.tests.js
+++ __tests__/lib/router.tests.js
@@ -8,4 +8,14 @@ describe('router', () => {
     expect(router.routes.POST).toEqual({});
     expect(router.routes.DELETE).toEqual({});
   });
+  it('accepts new routes with helpers', () => {
+    //Ask Instructor about refferencing here
+    router.get('/get', () => 'got');
+    expect(router.routes.GET['/get']).toBeDefined();
+    expect(router.routes.GET['/get']()).toBe('got');
+
+    router.post('/post', () => 'posted');
+    expect(router.routes.POST['/post']).toBeDefined();
+    expect(router.routes.POST['/post']()).toBe('posted');
+  });
 });
