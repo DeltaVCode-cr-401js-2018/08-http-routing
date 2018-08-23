3fece347a37cc50dbb57a46e6cf330168d040648
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-08-22 22:11:44 -0500

    added routes function to methods

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const router = exports;
 const routes = exports.routes = {};
 
 const methods = ['GET', 'POST', 'DELETE'];
 
 methods.forEach(method => {
   routes[method] = {};
+  router[method.toLowerCase()] = (path,callback) => {
+    routes[method][path] = callback;
+  };
+  
 });
